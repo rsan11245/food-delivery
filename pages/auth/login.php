2c358c8835446bc577482e05801a1fbc2a3b92f5
Roman Semenov <rsemenov150@gmail.com>
2024-03-02 18:09:03 +0300

    исправил верстку авторизации и регистрации

---
+++ pages/auth/login.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Вход</title>
+    <link rel="stylesheet" href="../../assets/css/auth.css">
+</head>
+<body>
+<div class="center">
+    <div class="form">
+        <h1>Вход</h1>
+        <form action="#" method="post">
+            <div class="txt_field">
+                <input type="tel" name="phone" id="phone" required>
+                <span></span>
+                <label for="phone">Телефон</label>
+            </div>
+            <div class="txt_field">
+                <input type="password" name="password" id="password" required>
+                <span></span>
+                <label for="password">Пароль</label>
+            </div>
+            <input type="submit" value="Войти">
+            <div class="signup_link">
+                <a href="#">Регистрация</a>
+            </div>
+        </form>
+
+    </div>
+</div>
+</body>
+</html>
---
+++ pages/auth/register.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Регистрация</title>
+    <link rel="stylesheet" href="../../assets/css/auth.css">
+</head>
+<body>
+<div class="center">
+    <div class="form">
+        <h1>Регистрация</h1>
+        <form action="#" method="post">
+            <div class="txt_field">
+                <input type="text" name="phone" id="phone" required>
+                <span></span>
+                <label for="phone">Телефон</label>
+            </div>
+            <div class="txt_field">
+                <input type="password" name="password" id="password" required>
+                <span></span>
+                <label for="password">Пароль</label>
+            </div>
+            <div class="txt_field">
+                <input type="password" name="confirm_password" id="confirm_password" required>
+                <span></span>
+                <label for="confirm_password">Повторите пароль</label>
+            </div>
+            <input type="submit" value="Регистрация">
+            <div class="signup_link">
+                <a href="#">Войти в существующий аккаунт</a>
+            </div>
+        </form>
+
+    </div>
+</div>
+</body>
+</html>
